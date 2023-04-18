7ca0f3f2ec9ead8aba0c6d3692726217a3b69136
Administrator <Administrator@501-10>
2023-04-18 20:18:22 +0900

    이광의 의 boardgame2 객체!

---
+++ BoardLife/src/main/java/vo/Boardgames2.java
@@ -0,0 +1,119 @@
+package vo;
+
+public class Boardgames2 {
+	private String b_id;
+    private String b_title_kor;
+    private String b_title_eng;
+    private String yearof;
+    private String pnum;
+    private String running_time;
+    private String age;
+    private double game_level;
+    private String sub_lang;
+    private String b_detail;
+    private String publisher;
+    private String geeklink;
+    private String designer;
+    private String b_theme;
+    private String proceed;
+    private String b_img;
+    
+    
+	public String getB_id() {
+		return b_id;
+	}
+	public void setB_id(String b_id) {
+		this.b_id = b_id;
+	}
+	public String getB_title_kor() {
+		return b_title_kor;
+	}
+	public void setB_title_kor(String b_title_kor) {
+		this.b_title_kor = b_title_kor;
+	}
+	public String getB_title_eng() {
+		return b_title_eng;
+	}
+	public void setB_title_eng(String b_title_eng) {
+		this.b_title_eng = b_title_eng;
+	}
+	public String getYearof() {
+		return yearof;
+	}
+	public void setYearof(String yearof) {
+		this.yearof = yearof;
+	}
+	public String getPnum() {
+		return pnum;
+	}
+	public void setPnum(String pnum) {
+		this.pnum = pnum;
+	}
+	public String getRunning_time() {
+		return running_time;
+	}
+	public void setRunning_time(String running_time) {
+		this.running_time = running_time;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public double getGame_level() {
+		return game_level;
+	}
+	public void setGame_level(double game_level) {
+		this.game_level = game_level;
+	}
+	public String getSub_lang() {
+		return sub_lang;
+	}
+	public void setSub_lang(String sub_lang) {
+		this.sub_lang = sub_lang;
+	}
+	public String getB_detail() {
+		return b_detail;
+	}
+	public void setB_detail(String b_detail) {
+		this.b_detail = b_detail;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getGeeklink() {
+		return geeklink;
+	}
+	public void setGeeklink(String geeklink) {
+		this.geeklink = geeklink;
+	}
+	public String getDesigner() {
+		return designer;
+	}
+	public void setDesigner(String designer) {
+		this.designer = designer;
+	}
+	public String getB_theme() {
+		return b_theme;
+	}
+	public void setB_theme(String b_theme) {
+		this.b_theme = b_theme;
+	}
+	public String getProceed() {
+		return proceed;
+	}
+	public void setProceed(String proceed) {
+		this.proceed = proceed;
+	}
+	public String getB_img() {
+		return b_img;
+	}
+	public void setB_img(String b_img) {
+		this.b_img = b_img;
+	}
+    
+}
